586eda7b3d8c78c6e415e7a1ccac2bc4d923d338
Your GitHub Name <your-email@example.com>
2026-05-18 14:44:29 +0700

    Show manual stock cuts in deduct table

--- app/stock-out/page.js
+++ app/stock-out/page.js
@@ -1,5 +1,5 @@
 import LegacyFrame from "@/components/LegacyFrame";
 
 export default function StockOutPage() {
-  return <LegacyFrame src="/legacy/stock_out.html?v=20260518-soldout-filter" title="Stock Out" />;
+  return <LegacyFrame src="/legacy/stock_out.html?v=20260518-manual-deduct-table" title="Stock Out" />;
 }
